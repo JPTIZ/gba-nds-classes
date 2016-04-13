957eb913496b46aa8a4177c5e71532775b8a5339
João Paulo Taylor Ienczak Zanette <profissional.jptiz@gmail.com>
2016-04-13 20:10:27 -0300

    screen tests

--- gba/1_exploring_mode_3/source/main.cpp
+++ gba/1_exploring_mode_3/source/main.cpp
@@ -1,6 +1,21 @@
 #include <cstdint>
 #include "gba.h"
 
+void wait() {
+    for (volatile auto j = 0u; j < 65535;) {
+        ++j;
+    };
+}
+
+void clear_screen() {
+    // clears screen
+    for (auto j = 0u; j < 240u; ++j) {
+        for (auto i = 0u; i < 160u; ++i) {
+            gba::video::vram(j, i, 0);
+        }
+    }
+}
+
 int main() {
     {
         using namespace gba::video;
@@ -8,8 +23,30 @@ int main() {
         lcd_control() = 3;
         enable_bg(2);
     }
-    for (auto i = 0u; i < 160u; ++i) {
-        gba::video::vram(i, i, 0x00FF);
+    auto first_y = true;
+    auto color = 1<<10u;
+    while (1) {
+        // fills screen nicely
+        if (first_y) {
+            for (auto i = 0u; i < 160u; ++i) {
+                for (auto j = 0u; j < 240u; ++j) {
+                    gba::video::vram(j, i, color);
+                }
+            }
+            wait();
+        } else {
+            // fills screen badly
+            for (auto j = 0u; j < 240u; ++j) {
+                for (auto i = 0u; i < 160u; ++i) {
+                    gba::video::vram(j, i, color);
+                }
+            }
+            wait();
+        }
+        wait();
+        if (color > 0x1F<<10u) {
+            color = 1<<10u;
+        }
+        color += 1<<10u;
     }
-    while (1);
 }
