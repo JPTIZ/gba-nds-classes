d1ce915b08f5c3eb02380f0a47295f69c47758d8
JPTIZ <jpaulotiz@gmail.com>
2017-06-14 17:10:55 -0300

    reading header ok

--- gba/4_bmp_files/src/bitmap.cpp
+++ gba/4_bmp_files/src/bitmap.cpp
@@ -1,14 +1,113 @@
 #include "bitmap.h"
 
+#include <cstring>
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <stdexcept>
 
 using converter::Bitmap;
+using converter::BitmapFileHeader;
+using converter::BitmapInfoHeader;
+
+
+auto load_header(std::ifstream& file) {
+    BitmapFileHeader header;
+    std::cout << "sizeof(header): " << sizeof(header) << '\n';
+    file.read(reinterpret_cast<char*>(&header), sizeof(header));
+    return std::move(header);
+}
+
+auto load_bmp_header(std::ifstream& file) {
+    BitmapInfoHeader header;
+    std::cout << "sizeof(header): " << sizeof(header) << '\n';
+    file.read(reinterpret_cast<char*>(&header), sizeof(header));
+    return std::move(header);
+}
+
+void show_data(const BitmapFileHeader& header) {
+    using std::setw;
+    std::cout <<
+        "|----------------------|\n"
+        "|  Bitmap File Header  |\n"
+        "|----------------------|\n"
+        "| File Type | " << setw(8) << header.file_type << " |\n"
+        "| File Size | " << setw(8) << header.file_size << " |\n"
+        "| DataBegin | " << setw(8) << header.data_begin << " |\n"
+        "|----------------------|\n";
+}
+
+auto method_name(converter::CompressionMethod method) {
+    using converter::CompressionMethod;
+    switch (method) {
+        case CompressionMethod::RGB:
+            return "RGB";
+        case CompressionMethod::RLE8:
+            return "RLE8";
+        case CompressionMethod::RLE4:
+            return "RLE4";
+        case CompressionMethod::BITFIELDS:
+            return "BITFIELDS";
+        case CompressionMethod::JPEG:
+            return "JPEG";
+        case CompressionMethod::PNG:
+            return "PNG";
+        case CompressionMethod::ALPHABITFIELDS:
+            return "ALPHABITFIELDS";
+        case CompressionMethod::CMYK:
+            return "CMYK";
+        case CompressionMethod::CMYKRLE8:
+            return "CMYKRLE8";
+        case CompressionMethod::CMYKRLE4:
+            return "CMYKRLE4";
+        default:
+            return "dunno";
+    }
+}
+
+void show_data(const BitmapInfoHeader& header) {
+    using std::setw;
+    std::cout <<
+        "|-------------------------------|\n"
+        "|       Bitmap Info Header      |\n"
+        "|-------------------------------|\n"
+        "| Header Size  | " << setw(14) << header.header_size << " |\n"
+        "| Width        | " << setw(14) << header.width << " |\n"
+        "| Height       | " << setw(14) << header.height << " |\n"
+        "| Color Planes | " << setw(14) << header.color_planes << " |\n"
+        "| BPP          | " << setw(14) << header.bpp << " |\n"
+        "| CompressMeth | " << setw(14) << method_name(header.compression_method) << " |\n"
+        "| Raw BMP Size | " << setw(14) << header.raw_size << " |\n"
+        "| Horizont.PPM | " << setw(14) << header.horizontal_ppm << " |\n"
+        "| Vertical PPM | " << setw(14) << header.vertical_ppm << " |\n"
+        "| Palette Size | " << setw(14) << header.palette_size << " |\n"
+        "| ImportantClr | " << setw(14) << header.important_colors << " |\n"
+        "|-------------------------------|\n";
+}
+
 
 Bitmap converter::load_bitmap(std::string filename) {
-    std::cout << "loading " << filename << '\n';
-
     std::ifstream file(filename, std::ios::binary);
+    if (not file.is_open()) {
+        throw std::invalid_argument{"file \"" + filename + "\" not found."};
+    }
+
+    auto file_header = load_header(file);
+    if (file_header.file_type != 0x4D42) {
+        throw std::invalid_argument{"file is not a valid bitmap."};
+    }
+
+    auto bmp_header = load_bmp_header(file);
+
+    show_data(file_header);
+    show_data(bmp_header);
+
+    for (auto i = 0u; i < bmp_header.width; ++i) {
+        converter::Color<16> pixel;
+        file.read(reinterpret_cast<char*>(&pixel), sizeof(pixel));
+        std::cout << *reinterpret_cast<std::uint16_t*>(&pixel.value) << '\n';
+    }
+
     return Bitmap{};
 }
--- gba/4_bmp_files/src/bitmap.h
+++ gba/4_bmp_files/src/bitmap.h
@@ -5,7 +5,49 @@
 
 namespace converter {
 
+struct BitmapFileHeader {
+    std::uint16_t file_type;
+    std::uint32_t file_size;
+    std::uint16_t reserved_06;
+    std::uint16_t reserved_08;
+    std::uint32_t data_begin;
+
+} __attribute__((packed));
+
+enum class CompressionMethod: std::uint32_t {
+    RGB = 0,
+    RLE8,
+    RLE4,
+    BITFIELDS,
+    JPEG,
+    PNG,
+    ALPHABITFIELDS,
+    CMYK = 11,
+    CMYKRLE8,
+    CMYKRLE4,
+};
+
+struct BitmapInfoHeader {
+    std::uint32_t header_size;
+    std::int32_t width;
+    std::int32_t height;
+    std::uint16_t color_planes{1};
+    std::uint16_t bpp{1};
+    CompressionMethod compression_method;
+    std::uint32_t raw_size{0};
+    std::int32_t horizontal_ppm;
+    std::int32_t vertical_ppm;
+    std::uint32_t palette_size{0};
+    std::uint32_t important_colors{0};
+} __attribute__((packed));
+
+template <std::size_t BPP>
+struct Color {
+    char value[BPP / 8];
+};
+
 struct Bitmap {
+
 };
 
 Bitmap load_bitmap(std::string filename);
